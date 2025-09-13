3ee77ff19e6cb220fe8839e055a6505fad6724f6
sankar-6 <sankarambati1266@gmail.com>
2025-09-14 00:54:14 +0530

    Update Factorial.java
    Enter a number : 5
    factorial is 120

--- Mathematics/Factorial.java
+++ Mathematics/Factorial.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Factorial {
+public class Factorial{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -16,3 +16,4 @@ public class Factorial {
     }
     
 }
+
